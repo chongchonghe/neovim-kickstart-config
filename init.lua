315b05e4211f453947fad1106e337b13c4c93e4a
chongchonghe <chongchong.he@anu.edu.au>
2025-09-13 21:01:49 +0800

    remove lsp

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ require('lazy').setup {
   require 'plugins.lualine',
   require 'plugins.treesitter',
   require 'plugins.telescope',
-  require 'plugins.lsp',
+  -- require 'plugins.lsp',
   -- require 'plugins.autocompletion',
   require 'plugins.gitsigns',
   -- require 'plugins.alpha',
