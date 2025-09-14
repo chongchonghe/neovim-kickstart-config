d163d693db17496334975846ba442d7cab65ded4
chongchonghe <chongchong.he@anu.edu.au>
2025-09-14 11:05:38 +0800

    add autocmplete

--- init.lua
+++ init.lua
@@ -22,7 +22,7 @@ require('lazy').setup {
   require 'plugins.treesitter',
   require 'plugins.telescope',
   -- require 'plugins.lsp',
-  -- require 'plugins.autocompletion',
+  require 'plugins.autocompletion',
   require 'plugins.gitsigns',
   -- require 'plugins.alpha',
   require 'plugins.indent-blankline',
