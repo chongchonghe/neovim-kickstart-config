bc28ec375fcc23d561127ffc7b16f27ea0ef1884
ChongChong He <chongchong.he@anu.edu.au>
2025-09-14 12:55:55 +1000

    add custom/

--- init.lua
+++ init.lua
@@ -32,3 +32,6 @@ require('lazy').setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- CUSTOM -- BY CHONGCHONG HE
+require('custom')
---
+++ lua/custom/init.lua
@@ -0,0 +1,4 @@
+-- ~/.config/nvim/lua/custom/init.lua
+-- Swap ; and : in normal mode for easier command entry
+vim.keymap.set('n', ';', ':')
+-- vim.keymap.set('n', ':', ';')
