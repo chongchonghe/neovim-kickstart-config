13afd8b80c53f88e55158d6785520986703de45a
ChongChong He <chongchong.he@anu.edu.au>
2025-10-04 02:38:55 +1000

    studio

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -2,3 +2,13 @@
 -- Swap ; and : in normal mode for easier command entry
 vim.keymap.set('n', ';', ':')
 -- vim.keymap.set('n', ':', ';')
+
+-- Simple toggle for absolute line numbers in the current buffer
+local function toggle_numbers()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local is_on = vim.api.nvim_buf_get_option(bufnr, "number")
+  vim.api.nvim_buf_set_option(bufnr, "number", not is_on)
+end
+
+vim.keymap.set("n", "<leader>l", toggle_numbers, { noremap = true, silent = true, desc = "Toggle line numbers" })
+
