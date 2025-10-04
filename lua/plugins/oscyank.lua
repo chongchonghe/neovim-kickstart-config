4c31d0643cf0d498d55ce2721ac8c289b0022627
ChongChong He <chongchong.he@anu.edu.au>
2025-10-04 21:18:20 +1000

    copy to clipboard over ssh

--- lua/plugins/oscyank.lua
+++ lua/plugins/oscyank.lua
@@ -3,6 +3,18 @@ return {
   "ojroques/vim-oscyank",
   init = function()
     vim.g.oscyank_silent = 1
+    
+    -- Automatically copy to clipboard using OSC 52 after any yank operation
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      group = vim.api.nvim_create_augroup("osc_yank", { clear = true }),
+      callback = function()
+        if vim.v.event.operator == 'y' then
+          vim.fn.OSCYankRegister('"')
+        end
+      end,
+    })
+    
+    -- Manual keymaps (as backup/alternative)
     -- Visual mode: send selection
     vim.keymap.set("x", "<leader>y", ":OSCYank<CR>", { silent = true })
     -- Normal mode: send + register
